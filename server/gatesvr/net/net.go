8320fdea2be24bb5c6085563d8d0a2108a0c668a
jianguo <jianguo@localhost.localdomain>
2025-04-06 11:22:51 +0800

    1

--- server/gatesvr/net/net.go
+++ server/gatesvr/net/net.go
@@ -1,17 +1,28 @@
 package jnet2
 
 import (
+	"jconfig"
 	"jhttp2"
 	"jhttps2"
 	"jnet"
 )
 
 func Init() {
-	jnet.Http.SetCodec(httpEncode, httpDecode)
-	jnet.Https.SetCodec(httpsEncode, httpsDecode)
-	jnet.Tcp.SetCodec(tcpEncode, tcpDecode)
-	jnet.Web.SetCodec(webEncode, webDecode)
-	jnet.Rpc.SetCodec(rpcEncode, rpcDecode)
-	jhttp2.Init()
-	jhttps2.Init()
+	if jconfig.Get("tcp") != nil {
+		jnet.Tcp.SetCodec(tcpEncode, tcpDecode)
+	}
+	if jconfig.Get("web") != nil {
+		jnet.Web.SetCodec(webEncode, webDecode)
+	}
+	if jconfig.Get("http") != nil {
+		jnet.Http.SetCodec(httpEncode, httpDecode)
+		jhttp2.Init()
+	}
+	if jconfig.Get("https") != nil {
+		jnet.Https.SetCodec(httpsEncode, httpsDecode)
+		jhttps2.Init()
+	}
+	if jconfig.Get("rpc") != nil {
+		jnet.Rpc.SetCodec(rpcEncode, rpcDecode)
+	}
 }
